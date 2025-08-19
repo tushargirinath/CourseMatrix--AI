18c10fbc7f4f8ef5ed183719b39d68d5497fb22a
Tushargirinath <tushargirinath26@gmail.com>
2025-08-20 02:01:01 +0530

    Initial commit

---
+++ Nextjs React/CourseMatrix-AI/app/_components/Header.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='flex justify-between'>
+        <Button> Get Started </Button>
+    </div>
+  )
+}
+
+export default Header
---
+++ Nextjs React/CourseMatrix-AI/app/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Header from "./_components/Header";
+
+export default function Home() {
+  return (
+   <div>
+     {/* HEADER */}
+      <Header/>
+     {/* Hero Section */}
+   </div>
+  );
+}
